a16031dba5c8705901dcb41612650aac6d74adb9
Sandeep <sandy5691@gmail.com>
2025-11-24 21:14:18 +0530

    commit into garg branch

--- src/test/java/in/ey/demotestng/Emp.java
+++ src/test/java/in/ey/demotestng/Emp.java
@@ -9,7 +9,7 @@ public class Emp {
 	String name; int salary;
     Emp(String n, int s){ name=n; salary=s; }
     public static void main(String[] args) {
-    	List<String> words = Arrays.asList("apple3", "banana", "watermelon", "kiwi");
+    	List<String> words = Arrays.asList("apple3", "banana", "watermelon", "kiwi2");
     	System.out.println(words.stream().collect(Collectors.toMap(str->str, String::length))
     	.entrySet().stream().max((o1,o2)->o1.getValue()>o2.getValue()?1:o1.getValue()<o2.getValue()?-1:0)
     	.get().getKey());
